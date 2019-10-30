c824de917ba604fe779513f5b0cf15883a21054c
Dmytro Afanasiev <DAfanasiev@luxoft.com>
2019-10-31 00:00:22 +0200

    Adds check running and files downloading

--- runchecks.sh
+++ runchecks.sh
@@ -1,14 +1,27 @@
-#FILES=`jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH"`/files
-#echo "Files = $FILES"
-echo $GITHUB_WORKSPACE
-cd $GITHUB_WORKSPACE
-ls
+#!/bin/bash
 
-#curl $FILES > files.json
-#FILES_URL=`jq -r '.[].raw_url' files.json`
+FILES_LINK=`jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH"`/files
+echo "Files = $FILES_LINK"
 
-#echo "File names: $FILES_URL"
+curl $FILES_LINK > files.json
+FILES_URLS_STRING=`jq -r '.[].raw_url' files.json`
 
-#mkdir files
-#cd files
-#curl $FILES_URL --remote-name-all
+readarray -t URLS <<<"$FILES_URLS_STRING"
+
+echo "File names: $URLS"
+
+mkdir files
+cd files
+for i in "${URLS[@]}"
+do
+   echo "Downloading $i"
+   curl -LOk --remote-name $i 
+done
+
+echo "Files downloaded!"
+echo "Performing checkup:"
+
+mkdir build
+cd build
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && make -j4
+clang-tidy ../*.cpp -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-#*,clang-analyzer-*,cppcoreguidelines-* -p=build
