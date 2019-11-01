7e2fc9630afe1351dfe1ab03339991aa0bf4489d
Dmytro <33125344+smay1613@users.noreply.github.com>
2019-11-02 00:50:22 +0200

    Delete old.sh

--- old.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-jq --raw-output . "$GITHUB_EVENT_PATH"
-#mkdir build
-#cd build
-#cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && make -j4
-#clang-tidy ../*.cpp -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-#*,clang-analyzer-*,cppcoreguidelines-* -p=$PWD
-
