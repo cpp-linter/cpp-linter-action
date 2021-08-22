98b1c1577f966558f105d9925c8558ae9029dc5c
Peter Shen <xianpeng.shen@gmail.com>
2021-08-22 01:35:55 -0600

    #8 update readme to suggest open push event by default (#9)
    * #8 update readme to suggest open push event by default
    
    * Push capable (#14)
    
    * Output exit code (#10)
    * solution for #7
    * parameterize EXIT_CODE
    * fix bash function calls; switch to the new docker image
    * fix bash array length syntax
    * oops need that apt update
    * ok now change the test code a bit
    * check output is working
    * fix bad yml syntax
    * is it being set at all?
    * use a string instead of an integer
    * fix bash logic syntax
    * single square brackets are the old way to do logic
    * review changes
    * add output var info to the readme
    * Update demo.cpp
    * Revert action to upstream master
    * fix dockerfile from image not exist
    * solution for #7
    * parameterize EXIT_CODE
    * fix bash function calls; switch to new docker image
    * check output is working
    * show me some env vars
    * trigger on push events
    * switch action to push-capable branch
    * echo the JSON that lives on the runner
    * use cat instead of echo
    * remove backticks
    * use alpine while debugging git events
    * Revert "use alpine while debugging git events"
    This reverts commit fbbb1b5a6f8a80f8d4245ebce07d7048ff74fedd.
    * test push compatibility
    * provide version input
    * docker uses tags
    * oops update run_checks.sh
    * fix jq args on push
    * test push src change
    * test src change w/ GH checkout
    * fix filename output for push events (w/ checkouts)
    * test new comment body
    * gotta break that bad habit
    * check against src change
    * use sed directly instead of sed then cat
    * tidy docs lied
    * don't know why sed isn't working
    * try multiple ARG options
    * try to replace the docker file
    * maybe without dyn var
    * bad domain?
    * nope needs a docker:// domain
    * reverting changes to docker file FROM
    * fix clang-format output to file
    * test on src changes
    * list clang-fmt warnings per file
    * add demo.h and adjust comment body
    * make test action use default extensions
    * don't checkout repo on test action
    * stay in working dir. DL into a hidden file
    * organize output better
    * adjust output again
    * fix output; switch back to checkout repo
    * enforce CPP syntax in the filename (no checkout)
    * sed taking too much out of the path in clang-tidy out
    * that's better (checkout is faster)
    * segregate script into functions
    * oops uncomment from debugging locally
    * show me a long diff
    * show me a split diff
    * clean-up (ready for PR)
    * update readme
    * remove the version from user inputs
    * only apply clang-tidy checks if it was specified
    * switch test action back to upstream
    * add version for user input
    
    Co-authored-by: Brendan <2bndy5@gmail.com>

--- runchecks.sh
+++ runchecks.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+# global varibales
 EXIT_CODE="0"
-PAYLOAD_FORMAT=""
 PAYLOAD_TIDY=""
+FENCES=$'\n```\n'
+OUTPUT=""
+URLS=""
+PATHNAMES=""
+declare -a JSON_INDEX
 
-function set_exit_code () {
+# alias CLI args
+args=("$@")
+FMT_STYLE=${args[0]}
+IFS=',' read -r -a FILE_EXT_LIST <<< "${args[1]}"
+TIDY_CHECKS="${args[2]}"
+cd "${args[3]}" || exit "1"
+CLANG_VERSION="${args[4]}"
+
+
+###################################################
+# Set the exit code (for expected exit calls).
+# Optional parameter overides action-specific logic
+###################################################
+set_exit_code () {
    if [[ $# -gt 0 ]]
    then
       EXIT_CODE="$1"
@@ -17,102 +35,234 @@ function set_exit_code () {
    echo "::set-output name=checks-failed::$EXIT_CODE"
 }
 
-# check for access token (ENV VAR needed for git API calls)
-if [[ -z "$GITHUB_TOKEN" ]]
-then
-	echo "The GITHUB_TOKEN is required."
-   set_exit_code "1"
-	exit "$EXIT_CODE"
-fi
+###################################################
+# Fetch JSON of event's changed files
+###################################################
+get_list_of_changed_files() {
+   echo "GH_EVENT_PATH = $GITHUB_EVENT_PATH"
+   echo "processing $GITHUB_EVENT_NAME event"
+   # cat "$GITHUB_EVENT_PATH" | jq '.'
 
-# parse CLI args
-args=("$@")
-FMT_STYLE=${args[0]}
-IFS=',' read -r -a FILE_EXT_LIST <<< "${args[1]}"
+   # Use git REST API payload
+   if [[ "$GITHUB_EVENT_NAME" == "push" ]]
+   then
+      FILES_LINK="$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA"
+   elif [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]
+   then
+      # FILES_LINK="$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/<PR ID number>/files"
+      # Get PR ID number from the event's JSON located in the runner's GITHUB_EVENT_PATH
+      FILES_LINK="$(jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH")/files"
+   fi
 
-# use git API payload
-FILES_LINK=`jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH"`/files
-echo "Files = $FILES_LINK"
+   # Download files list (another JSON containing files' names, URLS, statuses, & diffs/patches)
+   echo "Fetching files list from $FILES_LINK"
+   curl "$FILES_LINK" > .cpp_linter_action_changed_files.json
+}
 
-# setup download URLS
-curl $FILES_LINK > files.json
-FILES_URLS_STRING=`jq -r '.[].raw_url' files.json`
-readarray -t URLS <<<"$FILES_URLS_STRING"
+###################################################
+# extract info from downloaded JSON file
+###################################################
+extract_changed_files_info() {
+   # pull_request events have a slightly different JSON format than push events
+   JSON_FILES=".["
+   if [[ "$GITHUB_EVENT_NAME" == "push" ]]
+   then
+      JSON_FILES=".files["
+   fi
+   FILES_URLS_STRING=$(jq -r "$JSON_FILES].raw_url" .cpp_linter_action_changed_files.json)
+   FILES_NAMES_STRING=$(jq -r "$JSON_FILES].filename" .cpp_linter_action_changed_files.json)
 
-# exclude undesired files
-for index in "${!URLS[@]}"
-do
-  is_supported=0
-  for i in "${FILE_EXT_LIST[@]}"
-  do
-    if [[ ${URLS[index]} == *".$i" ]]
-    then
-      is_supported=1
-    fi
-  done
-  if [ $is_supported == 0 ]
-  then
-    unset -v "URLS[index]"
-  fi
-done
+   # convert json info to arrays
+   readarray -t URLS <<<"$FILES_URLS_STRING"
+   readarray -t PATHNAMES <<<"$FILES_NAMES_STRING"
 
-# exit early if nothing to do
-if [ ${#URLS[@]} == 0 ]
-then
-   set_exit_code "0"
-   echo "No source files need checking!"
-   exit $EXIT_CODE
-else
-   echo "File names: ${URLS[*]}"
-fi
+   # Initialize the `JSON_INDEX` array. This helps us keep track of the
+   # source files' index in the JSON after calling `filter_out_source_files()` function.
+   for index in "${!URLS[@]}"
+   do
+      # this will only be used when parsing diffs from the JSON
+      JSON_INDEX[$index]=$index
+   done
+}
 
-mkdir files
-cd files
-for i in "${URLS[@]}"
-do
-   echo "Downloading $i"
-   curl -LOk --remote-name $i
-done
+###################################################
+# exclude undesired files (specified by the user)
+###################################################
+filter_out_non_source_files() {
+   for index in "${!URLS[@]}"
+   do
+      is_supported=0
+      for i in "${FILE_EXT_LIST[@]}"
+      do
+         if [[ ${URLS[index]} == *".$i" ]]
+         then
+            is_supported=1
+            break
+         fi
+      done
 
-echo "Files downloaded!"
-echo "Performing checkup:"
-clang-tidy --version
+      if [ $is_supported == 0 ]
+      then
+         unset -v "URLS[index]"
+         unset -v "PATHNAMES[index]"
+         unset -v "JSON_INDEX[index]"
+      fi
+   done
 
-for i in "${URLS[@]}"
-do
-   filename=`basename $i`
-   clang-tidy $filename -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* >> clang-tidy-report.txt
-   clang-format -style="$FMT_STYLE" --dry-run -Werror "$filename" || echo "File: $filename not formatted!" >> clang-format-report.txt
-done
+   # exit early if nothing to do
+   if [ ${#URLS[@]} == 0 ]
+   then
+      set_exit_code "0"
+      echo "No source files need checking!"
+      exit $EXIT_CODE
+   else
+      echo "File names: ${PATHNAMES[*]}"
+   fi
+}
 
-PAYLOAD_TIDY=`cat clang-tidy-report.txt`
-PAYLOAD_FORMAT=`cat clang-format-report.txt`
-COMMENTS_URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
+###################################################
+# Download the files if not present.
+# This function assumes that the working directory is the root of the invoking repo.
+# Note that all github actions are run in path specified by the environment variable GITHUB_WORKSPACE.
+###################################################
+verify_files_are_present() {
+   # URLS, PATHNAMES, & PATCHES are parallel arrays
+   for index in "${!PATHNAMES[@]}"
+   do
+      if [[ ! -f "${PATHNAMES[index]}" ]]
+      then
+         echo "Downloading ${URLS[index]}"
+         curl --location --insecure --remote-name "${URLS[index]}"
+      fi
+   done
+}
 
-echo $COMMENTS_URL
-echo "Clang-tidy errors:"
-echo $PAYLOAD_TIDY
-echo "Clang-format errors:"
-echo $PAYLOAD_FORMAT
+###################################################
+# get the patch info from the JSON.
+# required parameter is the index in the JSON_INDEX array
+###################################################
+get_patch_info() {
+   # patches are multiline strings. Thus, they need special attention because of the '\n' used within.
+   #
+   # a git diff (aka "patch" in the REST API) can have multiple "hunks" for a single file.
+   # hunks start with `@@ -<start-line>,<number of lines> +<start-line>,<number of lines> @@`
+   # A positive sign indicates the incoming changes, while a negative sign indicates existing code that was changed
+   # Any changed lines will also have a prefixed `-` or `+`.
 
-if [ "$PAYLOAD_TIDY" != "" ]; then
-   OUTPUT=$'**CLANG-TIDY WARNINGS**:\n'
-   OUTPUT+=$'\n```\n'
-   OUTPUT+="$PAYLOAD_TIDY"
-   OUTPUT+=$'\n```\n'
-fi
+   file_status=$(jq -r "$JSON_FILES${JSON_INDEX[$1]}].status" .cpp_linter_action_changed_files.json)
 
-if [ "$PAYLOAD_FORMAT" != "" ]; then
-   OUTPUT=$'**CLANG-FORMAT WARNINGS**:\n'
-   OUTPUT+=$'\n```\n'
-   OUTPUT+="$PAYLOAD_FORMAT"
-   OUTPUT+=$'\n```\n'
-fi
+   # we only need the first line stating the line numbers changed (ie "@@ -1,5 +1,5 @@"")
+   patched_lines=$(jq -r -c "$JSON_FILES${JSON_INDEX[$1]}].patch" .cpp_linter_action_changed_files.json)
+   patches=$(echo "$patched_lines" | grep -o "@@ \\-[1-9]*,[1-9]* +[1-9]*,[1-9]* @@" | grep -o " +[1-9]*,[1-9]*" | tr -d "\\n" | sed 's; +;;; s;+;;g')
 
+   # if there is no patch field, we need to handle 'renamed' as an edgde case
+   if [[ "$patches" == "" ]]
+   then
+      echo "${PATHNAMES[$1]} was $file_status"
+      # don't bother checking renamed files with no changes to file's content
+      patches="0,0"
+   fi
+   echo "$patches"
+}
+
+###################################################
+# execute clang-tidy/format & assemble a unified OUTPUT
+###################################################
+capture_clang_tools_output() {
+   clang-tidy --version
+
+   for index in "${!URLS[@]}"
+   do
+      filename=$(basename ${URLS[index]})
+      if [[ -f "${PATHNAMES[index]}" ]]
+      then
+         filename="${PATHNAMES[index]}"
+      fi
+
+      true > clang_format_report.txt
+      true > clang_tidy_report.txt
+
+      echo "Performing checkup on $filename"
+      # echo "incoming changed lines: $(get_patch_info $index)"
+
+      if [ "$TIDY_CHECKS" == "" ]
+      then
+         clang-tidy-"$CLANG_VERSION" "$filename" >> clang_tidy_report.txt
+      else
+         clang-tidy-"$CLANG_VERSION" -checks="$TIDY_CHECKS" "$filename" >> clang_tidy_report.txt
+      fi
+      clang-format-"$CLANG_VERSION" -style="$FMT_STYLE" --dry-run "$filename" 2> clang_format_report.txt
+
+      if [[ $(wc -l < clang_tidy_report.txt) -gt 0 ]]
+      then
+         PAYLOAD_TIDY+=$"### ${PATHNAMES[index]}"
+         PAYLOAD_TIDY+="$FENCES"
+         sed -i "s|$GITHUB_WORKSPACE/||g" clang_tidy_report.txt
+         # cat clang_tidy_report.txt
+         PAYLOAD_TIDY+=$(cat clang_tidy_report.txt)
+         PAYLOAD_TIDY+="$FENCES"
+      fi
+
+      if [[ $(wc -l < clang_format_report.txt) -gt 0 ]]
+      then
+         if [ "$OUTPUT" == "" ]
+         then
+            OUTPUT=$'## Run `clang-format` on the following files\n'
+         fi
+         OUTPUT+="- [ ] ${PATHNAMES[index]}"$'\n'
+      fi
+   done
+
+   if [ "$PAYLOAD_TIDY" != "" ]; then
+      OUTPUT+=$'\n---\n## Output from `clang-tidy`\n'
+      OUTPUT+="$PAYLOAD_TIDY"
+   fi
+
+   echo "OUTPUT is:"
+   echo "$OUTPUT"
+}
+
+###################################################
+# POST action's results using REST API
+###################################################
+post_results() {
+   # check for access token (ENV VAR needed for git API calls)
+   if [[ -z "$GITHUB_TOKEN" ]]
+   then
+      set_exit_code "1"
+      echo "The GITHUB_TOKEN is required."
+      exit "$EXIT_CODE"
+   fi
+
+   COMMENTS_URL=$(jq -r .pull_request.comments_url "$GITHUB_EVENT_PATH")
+   if [[ "$GITHUB_EVENT_NAME" == "push" ]]
+   then
+      COMMENTS_URL="$FILES_LINK/comments"
+   fi
+
+   echo "COMMENTS_URL = $COMMENTS_URL"
+
+   PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
+
+   # creating PR comments is the same API as creating issue. Creating commit comments have more optional parameters (but same required API)
+   curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" "$COMMENTS_URL" --data "$PAYLOAD"
+}
+
+###################################################
+# The main body of this script (all function calls)
+###################################################
+# for local testing (without docker):
+#  1. Set the env var GITHUB_EVENT_NAME to "push" or "pull_request"
+#  2. Download and save the event's payload (in JSON) to a file named ".cpp_linter_action_changed_files.json".
+#       See the FILES_LINK variable in the get_list_of_changed_files() function for the event's payload.
+#  3. Comment out the following calls to `get_list_of_changed_files` & `post_results` functions
+#  4. Run this script using `./run_checks.sh <style> <extensions> <tidy checks> <relative working Dir>`
+###################################################
+
+get_list_of_changed_files
+extract_changed_files_info
+filter_out_non_source_files
+verify_files_are_present
+capture_clang_tools_output
 set_exit_code
-
-echo "OUTPUT is: \n $OUTPUT"
-
-PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
-
-curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "$PAYLOAD" "$COMMENTS_URL"
+post_results
