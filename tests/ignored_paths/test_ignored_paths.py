cd60a491e0871d05df324b5a3ea1f69b64adaa0f
Brendan <2bndy5@gmail.com>
2022-07-26 19:05:33 -0700

    run pytest in CI
    - add pytest to dev reqs
    - rename run-pylint.yml to run-dev-tests.yml
    - install cpp-linter pkg in run-dev-tests.yml workflow
    - upgrade actions/setup-python to v2 in run-dev-tests.yml workflow
    - rename step in run-dev-tests.yml workflow

--- tests/ignored_paths/test_ignored_paths.py
+++ tests/ignored_paths/test_ignored_paths.py
@@ -1,15 +1,22 @@
+"""Tests that focus on the ``ignore`` option's parsing."""
 import os
 from cpp_linter.run import parse_ignore_option
 
+
 def test_ignored():
+    """test ignoring of a specified path."""
     ignored, not_ignored = parse_ignore_option("src")
     assert "src" in ignored and not not_ignored
 
+
 def test_not_ignored():
+    """test explicit inclusion of a path and ignore the root path."""
     ignored, not_ignored = parse_ignore_option("!src|")
     assert "src" in not_ignored and "" in ignored
 
+
 def test_ignore_submodule():
+    """test auto detection of submodules and ignore the paths appropriately."""
     os.chdir(os.path.split(__file__)[0])
     ignored, not_ignored = parse_ignore_option("!pybind11")
     for ignored_submodule in ["RF24", "RF24Network", "RF24Mesh"]:
