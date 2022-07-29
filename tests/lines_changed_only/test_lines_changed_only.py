4b4e18ad8fe4141d596d31d6bd13bcf1ef179e65
Brendan <2bndy5@gmail.com>
2022-07-28 18:51:43 -0700

    parametrize  unit tests

--- tests/lines_changed_only/test_lines_changed_only.py
+++ tests/lines_changed_only/test_lines_changed_only.py
@@ -1,10 +1,11 @@
 """Various tests realated to the ``lines_changed_only`` option."""
 import os
 import logging
+from typing import Dict, Any, cast
 from pathlib import Path
 import json
 import re
-from pytest import LogCaptureFixture
+import pytest
 import cpp_linter
 from cpp_linter.clang_format_xml import parse_format_replacements_xml
 from cpp_linter.run import (
@@ -46,66 +47,40 @@ def test_lines_changed_only():
         raise RuntimeError("test failed to find files")
 
 
-def test_run_clang_format_diff_only(caplog: LogCaptureFixture):
+TEST_REPO = re.compile(
+    "https://api.github.com/repos/(?:\\w|\\-|_)+/((?:\\w|\\-|_)+)/.*"
+)
+
+
+@pytest.mark.parametrize("lines_changed_only", [1, 2])
+def test_run_clang_format_on_diff(
+    caplog: pytest.LogCaptureFixture, lines_changed_only: int
+):
     """Using the output (expected_result.json) from ``test_lines_changed_only()``,
-    run clang-format with lines-changed-only set to 'true'.
+    run clang-format with lines-changed-only set to accordingly.
 
-    This will make sure clang-format warnings are filtered to
-    only lines in the diff's chunks."""
-    # log_commander.setLevel(logging.INFO)
-    caplog.set_level(logging.INFO, logger=log_commander.name)
-    lines_changed_only = 1  # 1 means entire diff chunks
-    style = "file"  # this test includes a custom style guide
-    os.chdir(os.path.split(__file__)[0])
-    cpp_linter.Globals.FILES = json.loads(
-        Path("expected_result.json").read_text(encoding="utf-8")
-    )
-    verify_files_are_present()
-    for file in cpp_linter.Globals.FILES["files"]:
-        filename: str = file["filename"]
-        run_clang_format(
-            filename=filename,
-            file_obj=file,
-            version="",
-            style=style,
-            lines_changed_only=lines_changed_only,
-        )
-        if os.path.getsize("clang_format_output.xml"):
-            parse_format_replacements_xml(filename.replace("/", os.sep))
-    make_annotations(
-        style=style, file_annotations=True, lines_changed_only=lines_changed_only
-    )
-    record_lines = re.compile(r"\(lines (.*)\)")
-    record_file = re.compile(r"File (.*)\s")
-    for record in caplog.records:
-        if record_lines.search(record.message) is not None:
-            lines = [
-                int(l.strip())
-                for l in record_lines.sub("\\1", record.message).split(",")
-            ]
-            filename = record_file.sub("\\1", record.message)
-            for file in cpp_linter.Globals.FILES["files"]:
-                if file["filename"] == filename:
-                    ranges = file["line_filter"]["diff_chunks"]
-                    for line in lines:
-                        assert line in [range(r[0], r[1]) for r in ranges]
-                    break
+    tested input values:
 
-
-def test_run_clang_format_diff_adds(caplog: LogCaptureFixture):
-    """Using the output (expected_result.json) from ``test_lines_changed_only()``,
-    run clang-format with lines-changed-only set to 'true'.
+    - 1 = means entire diff chunks
+    - 2 = means only lines in diff containing additions
 
     This will make sure clang-format warnings are filtered to
     only lines in the diff containing additions."""
-    # log_commander.setLevel(logging.INFO)
     caplog.set_level(logging.INFO, logger=log_commander.name)
-    lines_changed_only = 2  # 2 means only lines in diff containing additions
     style = "file"  # this test includes a custom style guide
-    os.chdir(os.path.split(__file__)[0])
-    cpp_linter.Globals.FILES = json.loads(
-        Path("expected_result.json").read_text(encoding="utf-8")
+    range_focus = "lines_added"
+    if lines_changed_only == 1:
+        range_focus = "diff_chunks"
+    test_root = os.path.split(__file__)[0]
+    os.chdir(test_root)
+    cpp_linter.Globals.FILES = cast(
+        Dict[str, Any],
+        json.loads(Path("expected_result.json").read_text(encoding="utf-8")),
     )
+    repo_root = TEST_REPO.sub("\\1", cpp_linter.Globals.FILES["url"])
+    if not os.path.exists(repo_root):
+        os.mkdir(repo_root)
+    os.chdir(os.path.join(test_root, repo_root))
     verify_files_are_present()
     for file in cpp_linter.Globals.FILES["files"]:
         filename: str = file["filename"]
@@ -132,7 +107,7 @@ def test_run_clang_format_diff_adds(caplog: LogCaptureFixture):
             filename = record_file.sub("\\1", record.message)
             for file in cpp_linter.Globals.FILES["files"]:
                 if file["filename"] == filename:
-                    ranges = file["line_filter"]["lines_added"]
+                    ranges = file["line_filter"][range_focus]
                     for line in lines:
                         assert line in [range(r[0], r[1]) for r in ranges]
                     break
--- tests/test_cli_args.py
+++ tests/test_cli_args.py
@@ -1,116 +1,75 @@
 """Tests related parsing input from CLI arguments."""
-from typing import List
+from typing import List, Union
+import pytest
 from cpp_linter.run import cli_arg_parser
 
 
-class TestArgs:
-    """Grouped tests using a common namespace declaration"""
+class Args:
+    """A pseudo namespace declaration. Each attribute is initialized with the
+    corresponding CLI arg's default value."""
 
-    class Args:
-        """A pseudo namespace declaration. Each attribute is initialized with the
-        corresponding CLI arg's default value."""
-
-        verbosity: int = 10
-        database: str = ""
-        style: str = "llvm"
-        tidy_checks: str = (
-            "boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,"
-            "clang-analyzer-*,cppcoreguidelines-*"
-        )
-        version: str = ""
-        extensions: List[str] = [
-            "c",
-            "h",
-            "C",
-            "H",
-            "cpp",
-            "hpp",
-            "cc",
-            "hh",
-            "c++",
-            "h++",
-            "cxx",
-            "hxx",
-        ]
-        repo_root: str = "."
-        ignore: str = ".github"
-        lines_changed_only: int = 0
-        files_changed_only: bool = False
-        thread_comments: bool = False
-        file_annotations: bool = True
-
-    def test_defaults(self):
-        """test default values"""
-        args = cli_arg_parser.parse_args("")
-        expected = self.Args()
-        for key in args.__dict__.keys():
-            assert args.__dict__[key] == getattr(expected, key)
-
-    def test_verbosity(self):
-        """test verbosity option"""
-        args = cli_arg_parser.parse_args(["--verbosity=20"])
-        assert args.verbosity == 20
-
-    def test_database(self):
-        """test database option"""
-        args = cli_arg_parser.parse_args(["--database=build"])
-        assert args.database == "build"
-
-    def test_style(self):
-        """test style option"""
-        args = cli_arg_parser.parse_args(["--style=file"])
-        assert args.style == "file"
-
-    def test_tidy_checks(self):
-        """test tidy-checks option"""
-        args = cli_arg_parser.parse_args(["--tidy-checks=-*"])
-        assert args.tidy_checks == "-*"
-
-    def test_version(self):
-        """test version option"""
-        args = cli_arg_parser.parse_args(["--version=14"])
-        assert args.version == "14"
-
-    def test_extensions(self):
-        """test extensions option"""
-        args = cli_arg_parser.parse_args(["--extensions", ".cpp, .h"])
-        assert args.extensions == ["cpp", "h"]
-        args = cli_arg_parser.parse_args(["--extensions=cxx, .hpp"])
-        assert args.extensions == ["cxx", "hpp"]
-
-    def test_repo_root(self):
-        """test repo-root option"""
-        args = cli_arg_parser.parse_args(["--repo-root=src"])
-        assert args.repo_root == "src"
-
-    def test_ignore(self):
-        """test ignore option"""
-        args = cli_arg_parser.parse_args(["--ignore=!src|"])
-        assert args.ignore == "!src|"
-
-    def test_lines_changed_only(self):
-        """test lines-changed-only option"""
-        args = cli_arg_parser.parse_args(["--lines-changed-only=True"])
-        assert args.lines_changed_only == 1
-        args = cli_arg_parser.parse_args(["--lines-changed-only=strict"])
-        assert args.lines_changed_only == 2
-
-    def test_files_changed_only(self):
-        """test files-changed-only option"""
-        args = cli_arg_parser.parse_args(["--files-changed-only=True"])
-        assert args.files_changed_only is True
-
-    def test_thread_comments(self):
-        """test thread-comments option"""
-        args = cli_arg_parser.parse_args(["--thread-comments=True"])
-        assert args.thread_comments is True
-
-    def test_file_annotations(self):
-        """test file-annotations option"""
-        args = cli_arg_parser.parse_args(["--file-annotations=False"])
-        assert args.file_annotations is False
+    verbosity: int = 10
+    database: str = ""
+    style: str = "llvm"
+    tidy_checks: str = (
+        "boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,"
+        "clang-analyzer-*,cppcoreguidelines-*"
+    )
+    version: str = ""
+    extensions: List[str] = [
+        "c",
+        "h",
+        "C",
+        "H",
+        "cpp",
+        "hpp",
+        "cc",
+        "hh",
+        "c++",
+        "h++",
+        "cxx",
+        "hxx",
+    ]
+    repo_root: str = "."
+    ignore: str = ".github"
+    lines_changed_only: int = 0
+    files_changed_only: bool = False
+    thread_comments: bool = False
+    file_annotations: bool = True
 
 
-if __name__ == "__main__":
-    test = TestArgs()
-    test.test_defaults()
+def test_defaults():
+    """test default values"""
+    args = cli_arg_parser.parse_args("")
+    for key in args.__dict__.keys():
+        assert args.__dict__[key] == getattr(Args, key)
+
+
+@pytest.mark.parametrize(
+    "arg_name,arg_value,attr_name,attr_value",
+    [
+        ("verbosity", "20", "verbosity", 20),
+        ("database", "build", "database", "build"),
+        ("style", "file", "style", "file"),
+        ("tidy-checks", "-*", "tidy_checks", "-*"),
+        ("version", "14", "version", "14"),
+        ("extensions", ".cpp, .h", "extensions", ["cpp", "h"]),
+        ("extensions", "cxx,.hpp", "extensions", ["cxx", "hpp"]),
+        ("repo-root", "src", "repo_root", "src"),
+        ("ignore", "!src|", "ignore", "!src|"),
+        ("lines-changed-only", "True", "lines_changed_only", 1),
+        ("lines-changed-only", "stricT", "lines_changed_only", 2),
+        ("files-changed-only", "True", "files_changed_only", True),
+        ("thread-comments", "True", "thread_comments", True),
+        ("file-annotations", "False", "file_annotations", False),
+    ],
+)
+def test_arg_parser(
+    arg_name: str,
+    arg_value: str,
+    attr_name: str,
+    attr_value: Union[int, str, List[str], bool],
+):
+    """parameterized test of specific args compared to their parsed value"""
+    args = cli_arg_parser.parse_args([f"--{arg_name}={arg_value}"])
+    assert getattr(args, attr_name) == attr_value
