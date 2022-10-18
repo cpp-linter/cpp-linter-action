ecd50489d7807a8e031a020c7b2f676026470c73
shenxianpeng <xianpeng.shen@gmail.com>
2022-10-18 14:06:56 +0800

    Refactor action code

---
+++ src/venv.sh
@@ -0,0 +1,51 @@
+# Create temp Python virtual environment
+# to fix https://github.com/cpp-linter/cpp-linter-action/issues/111
+# This script can be decommissioned when
+# resolve https://github.com/cpp-linter/clang-tools-pip/issues/15
+
+usage() {
+cat << EOF
+
+Usage: sh $0 [param]
+Create and activate Python virtual environment
+
+Parameters:
+    create: Create Python virtual environment
+    activate: Activate Python virtual environment
+    delete: Delete Python virtual environment
+
+Examples:
+    $ sh $0 create
+    $ sh $0 activate
+    $ sh $0 delete
+EOF
+exit 0
+}
+
+
+create(){
+    python3 -m venv venv
+}
+
+delete(){
+    rm -rf venv
+}
+
+activate(){
+    if [[ "${{runner.os}}"  == "Windows" ]];then
+        ./venv/Scripts/activate
+    else
+        source ./venv/bin/activate
+    fi
+}
+
+#=============#
+# MAIN starts #
+#=============#
+param=$1
+case "$param" in
+    *create*)   create ;;
+    *activate*) activate ;;
+    *delete*)   delete ;;
+    *)          usage ;;
+esac
